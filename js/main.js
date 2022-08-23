ed985b576d13e3c05777a6346e4fbbf771649cf4
Nika-Stanic <nika.stanic11@gmail.com>
2022-08-23 09:44:01 +0200

    fini

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  Nika Stanic
  * @version 0.1
- * @since   2019-08-19
+ * @since   2022-08-23
  */
 
 (function main() {
-  'use strict'; // Demande un interprétation stricte du code
+    'use strict'; // Demande un interprétation stricte du code
 
-  let a = 'Bonjour';
+    let valcelsius = Number(prompt('Température en Celsius : '));
+    let valFahrenheit = (valcelsius * 9) / 5 + 32;
 
-  console.log(a);
+    alert(`${valcelsius}[°C] = ${valFahrenheit}[°F]`);
+
+
 }()); // Main IIFE
 
